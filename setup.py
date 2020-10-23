457d179f0cc054762137a3e7ba6c0bc05660acb5
BerkWerk <if18b189@technikum-wien.at>
2020-10-23 12:22:40 +0200

    testing automatic versioning

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ setup(
     name='PythonClingo_Interface',
     version='1.0.0',
     extras_require=dict(tests=['pytest']),
-    packages=find_packages(where="src"),
+    packages=find_packages(where="src", exclude=['tests', 'tests.*']),
     package_dir={"": "src"},
     url='https://github.com/BerkWerk/PythonClingo_Interface',
     license='',
     author='Oskar & Berk',
     author_email='',
     description='A library to interface python(especially jupyter notebook) code with clingo',
-    # automatic versioning
+    # install_requires=['numpy==1.8.1'],  #  package dependencies
+    # entry_points={'console_scripts': ['run = mypackage.module1:run']},  # useful if you want to run from terminal/console (functions made available as command-line tools)
+    ### automatic versioning
     use_scm_version=True,
     setup_requires=['setuptools_scm']
 )
-
-
