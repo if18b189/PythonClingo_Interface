fd904d571b3d265d07176948e12ea1e6f8820090
BerkWerk <if18b189@technikum-wien.at>
2020-10-23 12:48:00 +0200

    testing automatic versioning

--- src/pythonclingointerface/__version__.py
+++
@@ -1,4 +0,0 @@
-__title__ = 'PythonClingo_Interface'
-__description__ = 'A Python library for automating interaction with Clingo'
-__github_url__ = 'https://github.com/BerkWerk/PythonClingo_Interface'
-__version__ = '1.0.0-dev'
