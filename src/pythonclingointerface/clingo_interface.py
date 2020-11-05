766fc16779ecd8be2106f49ca360062061f921b5
WiczusFH <if19b078@technikum-wien.at>
2020-11-06 00:13:31 +0100

    Subprocess call added to the Clingo example under ClingoInterfaceRun()

--- src/pythonclingointerface/clingo_interface.py
+++ src/pythonclingointerface/clingo_interface.py
@@ -1,24 +1,17 @@
 import subprocess
-
-# class ClingoInterface:
-#
-#     def __init__(self, text):
-#         self.text = text
-#
-#     def execute(self):
-#         return self.text
-
-    # def addFact(self,varName,var):
-    #     return
+import os
 
 
-# output for testing purpose only
-# obj = ClingoInterface("hello world")
-# print(obj.execute())
 
-#Clingo
-# subprocess.call(['Exes/Executable.exe','hello','to you'])
 
-var=subprocess.call('clingo')
-print(type(var))
+class ClingoInterface:
+
+    def __init__(self, text):
+        self.text = text
+
+    def execute(self):
+        return self.text
+
+    def run(self):
+        subprocess.call('clingo ../../tests/clingoCodeExample')
 
