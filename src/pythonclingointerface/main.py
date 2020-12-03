ed53c01ee001b39d792b2aac7b69b61fac35ec59
BerkWerk <if18b189@technikum-wien.at>
2020-12-03 14:45:49 +0100

    clingo library can now be accessed and run from jupyter notebook files
    reworked/updated jupyterParenthesis() to check several .ipynb, store their clingo code in tempfiles and return paths to all tempfiles as a list
    
    run() now only check the datatype provided as an argument and decides how to execute the code
    
    executeClingoCode() executes the actual code, basically does what run() used to do in earlier versions
    
    added the function findDirectory() because running the program from jupyterNotebooks and main() resulted in a problem. jupyterParenthesis() was looking for .ipynb files 1 directory above when run from jupyterNotebooks.

--- src/pythonclingointerface/main.py
+++ src/pythonclingointerface/main.py
@@ -1,8 +1,23 @@
-from pythonclingointerface import ClingoInterface
+from pythonclingointerface import clingo_interface
 
 
 def main():
-    ClingoInterface("text")
+    # test = clingo_interface.ClingoInterface()
+    # path = test.jnotebookParenthesis()
+    # test.run(path)
+
+    test = clingo_interface.ClingoInterface()
+    path = test.jupyterParenthesis()
+    test.run(path)
+    test.printSolutions()
+
+    # path = os.path.abspath("")
+    #
+    # print("absolute: " + path)
+    # print("old: " + os.path.join(os.path.abspath(".\\PycharmProjects")))
+    #
+    # print(findDirectory("PythonClingo_Interface"))
+
     # ClingoInterface("text").run("clingoCodeExample")
 
 
