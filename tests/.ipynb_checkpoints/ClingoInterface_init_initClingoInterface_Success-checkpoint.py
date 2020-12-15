034f63d72fa39380062d37bb55e63097fe972de2
WiczusFH <71842990+WiczusFH@users.noreply.github.com>
2020-12-15 16:59:56 +0100

    added clingo Problem class

---
+++ tests/.ipynb_checkpoints/ClingoInterface_init_initClingoInterface_Success-checkpoint.py
@@ -0,0 +1,8 @@
+# first test ... resources: https://www.jetbrains.com/pycharm/guide/tutorials/visual_pytest/hello_test/
+#                           https://www.thecodebuzz.com/tdd-unit-testing-naming-conventions-and-standards/
+from pythonclingointerface import ClingoInterface
+
+
+def test_execute():
+    test = ClingoInterface("test")
+    assert test.execute() == "test"
