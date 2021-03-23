50c489214deac9c415694d9fa36852d0516d4278
if18b189 <49987011+if18b189@users.noreply.github.com>
2021-03-23 16:32:00 +0100

    added test.ipynb added new requirements.txt (pigar) changed some code

--- tests/pyTest.py
+++ tests/pyTest.py
@@ -1,8 +1,5 @@
 from src.pythonclingointerface.clingo_interface import ClingoInterface
 
-
-
-
-test=ClingoInterface()
-test.checkParenthesis(py=False,txt=False, ipynb=True)
+test = ClingoInterface()
+test.checkParenthesis(py=False, txt=False, ipynb=True)
 test.printSolutions()
